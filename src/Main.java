0ba8aa80ace69050d26d041bc927f86ff6ce07ac
fkj <fkj@eamv.dk>
2026-04-16 01:19:28 +0200

    changed output text again

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    String message = "Hello World";
+    String message = "Hello World of Git";
 
     System.out.println(message);
   }
